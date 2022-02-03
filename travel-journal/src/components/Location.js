947d02dfc35ebd206d098a864535094a23dd294e
Owais Khan <45107209+kowais915@users.noreply.github.com>
2022-02-03 23:34:16 +0500

    added location icon and styled the cards further

--- travel-journal/src/components/Location.js
+++ travel-journal/src/components/Location.js
@@ -1,15 +1,19 @@
+import icon from "../images/location.png"
+
 const Location = (props) => {
     return (
         <main className = "location">
           <div className="image">
-              <img></img>
+            <img src={props.item.imageUrl}/>
+            
+   
           </div>
           
           <div className="info">
-            <span>location and the google maps link</span>
-            <h1>Title</h1>
-            <p>Date</p>
-            <p>Description</p>
+            <span className="icon"><img  src={icon}/>{props.item.location}  <a href={props.item.googleMapsUrl}>Google Maps URL</a></span>
+            <h1>{props.item.title}</h1>
+            <p><strong>{props.item.startDate} - {props.item.endDate}</strong></p>
+            <p>{props.item.description}</p>
           </div>
 
         </main>
